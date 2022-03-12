2acd206c78f82b2543ccb6885155e85cfd1f767f
kdn2517 <rab7777777rab@gmail.com>
2022-03-12 15:49:17 +0300

    связи моделей + немного админки

--- my_site/news/models.py
+++ my_site/news/models.py
@@ -5,8 +5,10 @@ class News(models.Model):
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    photo = models.ImageField(upload_to='photo/%Y/%m/%d/', verbose_name='Фото')
+    photo = models.ImageField(upload_to='photo/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано?')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True,
+                                 verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -15,3 +17,15 @@ class News(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
