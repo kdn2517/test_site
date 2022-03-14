8164f2ac4118086933432df0e87970e6840b880a
kdn2517 <rab7777777rab@gmail.com>
2022-03-14 16:17:09 +0300

    кастомные валидаторы

--- my_site/news/forms.py
+++ my_site/news/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import News
-
+import re
+from django.core.exceptions import ValidationError
 
 class NewsForm(forms.ModelForm):
     class Meta:
@@ -12,3 +13,9 @@ class NewsForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
             'category': forms.Select(attrs={'class': 'form-control'}),
         }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
