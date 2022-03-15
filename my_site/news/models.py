b6887d3843db16bcfb8d2e3c836de4c78f832969
kdn2517 <rab7777777rab@gmail.com>
2022-03-15 14:21:19 +0300

    polls

--- my_site/news/models.py
+++ my_site/news/models.py
@@ -27,7 +27,7 @@ class News(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
 
-    def get_absolute_url(self):
+    def get_absolute_url(self): 
         return reverse('category', kwargs={'category_id': self.pk})
 
     def __str__(self):
