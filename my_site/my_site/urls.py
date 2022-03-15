1e55d427fe300e0e3dd0b9f9a8aaed147073d057
kdn2517 <rab7777777rab@gmail.com>
2022-03-15 15:31:29 +0300

    ckeditor

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
-    path('polls', include('polls.urls'))
+    path('polls', include('polls.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
--- my_site/news/models.py
+++ my_site/news/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.urls import reverse
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Наименование')
-    content = models.TextField(blank=True, verbose_name='Контент')
+    content = RichTextUploadingField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photo/%Y/%m/%d/', verbose_name='Фото', blank=True,
