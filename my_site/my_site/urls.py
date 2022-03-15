2e8ac1e0b4beea6ea941a2fb5380d26fa8713d78
kdn2517 <rab7777777rab@gmail.com>
2022-03-15 18:48:00 +0300

    шаблон

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
-    path('polls', include('polls.urls')),
+    path('polls/', include('polls.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
